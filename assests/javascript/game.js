8d2df9944b485588611f8c48fa6afe6b487b25c3
Little Milo <no-reply>
2019-03-18 19:23:26 -0700

    added css

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,3 +1,4 @@
+// computers array
 var computerChoices= ["a", "b", "c", "d", "e", "f", "g", "h",
 "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
 "v", "w", "x", "y", "z"];
@@ -21,12 +22,22 @@ document.onkeyup = function(event) {
     // Shows computer guess, then random choice based on array length.
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
+    // user array
     var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
     "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", 
     "x", "y", "z",];
 		 	
 
 	if (options.indexOf(userGuess) > -1) {
+    
+
+    var html = "<p>Guess what letter I'm thinking of!</p>" +
+    "<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>" +
+    "<p>Guesses left: " + guessesLeft + "</p>" +
+    "<p>Your guesses so far: " + lettersGuessed + "</p>";
+
+  document.querySelector("#game").innerHTML = html;
 
     
     // if user guess === computer guess, user wins
@@ -36,7 +47,7 @@ document.onkeyup = function(event) {
 	    lettersGuessed.length = 0;
 	    computerGuess.length = 0;    
 }
-
+    
      else if ((userGuess !==  computerGuess[0])  && (guessesLeft > 0)) {
         guessesLeft = guessesLeft-1;
     }  
@@ -47,16 +58,5 @@ document.onkeyup = function(event) {
         lettersGuessed.length = 0;
         computerGuess.length = 0;    
     }
-
-
-
-    var html = "<p>Guess what letter I'm thinking of!</p>" +
-          "<p>Wins: " + wins + "</p>" +
-          "<p>Losses: " + losses + "</p>" +
-          "<p>Guesses left: " + guessesLeft + "</p>" +
-          "<p>Your guesses so far: " + lettersGuessed + "</p>";
- 
-    document.querySelector("#game").innerHTML = html;
-
     }
 };
